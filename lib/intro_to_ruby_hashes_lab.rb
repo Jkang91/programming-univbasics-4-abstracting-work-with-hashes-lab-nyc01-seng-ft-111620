e81805649bdc1c93dd0e50a30d73f965edf16c76
June Young Kang <jkang34@gmail.com>
2020-10-13 23:35:13 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -10,3 +10,8 @@ def update_counting_hash(hash, key)
   hash[key] ? hash[key] += 1: hash[key] = 1
   hash
 end
+
+def update_counting_hash(hash, key)
+  hash[key] ? hash[key] += 1: hash[key] = 1
+  hash
+end
